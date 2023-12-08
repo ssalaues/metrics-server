1e3066889b16bd02c4b7f6a665789da8139ec5c6
Salim Salaues <salimsalaues@gmail.com>
2023-12-07 20:45:06 -0500

    feat: cache metrics

--- server.go
+++ server.go
@@ -5,11 +5,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const port string = "1234" // TODO get from env, default to 1234
+const interval time.Duration = 1 // TODO make this configurable from env
+type MetricsCache struct {
+	Metrics	string
+}
 
 func ReadMetricsFromFile(file string) string {
+	log.Println("Reading metrics file")
 	data, err := os.ReadFile(file)
 	if err != nil {
 		panic(err) // TODO possibly make soft fail and just log instead
@@ -17,19 +23,40 @@ func ReadMetricsFromFile(file string) string {
 	return string(data)
 }
 
-func MetricsRoute(w http.ResponseWriter, r *http.Request) {
+func (m *MetricsCache) UpdateMetricsCache() chan string {
+	ticker := time.NewTicker(interval * time.Second)
+	quit := make(chan string)
+	go func() {
+		for {
+			select {
+				case <- ticker.C:
+					m.Metrics = ReadMetricsFromFile("metrics_from_special_app.txt")
+				case <- quit:
+					ticker.Stop()
+					return
+			}
+		}
+	} ()
+	return quit
+}
+
+func (m *MetricsCache) MetricsRoute(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Fprintf(w, ReadMetricsFromFile("metrics_from_special_app.txt"))
+	log.Println("GET /metrics")
+	fmt.Fprintf(w, m.Metrics)
 }
 
 func main() {
 	log.Println("Metrics server starting up")
 
 	// add new routes here
-	http.HandleFunc("/metrics", MetricsRoute)
+	metrics := &MetricsCache{""}
+	metrics.UpdateMetricsCache()
+	http.HandleFunc("/metrics", metrics.MetricsRoute)
+
 
 	log.Println("Metrics server started on port", port)
 
