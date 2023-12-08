319959cb6abdd2e9262a049627f0cd21147fd958
Salim Salaues <salimsalaues@gmail.com>
2023-12-08 01:13:45 -0500

    fix: grep first match

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -ex
 LOCATION=${1:-/usr/local/bin}
 
 # if it's already installed kill the sub process so the systemd service stops quicker/reliably
-kill $(ps aux | grep generate-data.sh | awk '{ print $2 }') || true
+kill $(ps aux | grep -m1 generate-data.sh | awk '{ print $2 }') || true
 systemctl stop metrics.service || true
 
 go build
