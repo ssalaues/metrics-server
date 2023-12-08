f3cdc790ba37f9d9adaaf18be7f5334bc3dfecd6
Salim Salaues <salimsalaues@gmail.com>
2023-12-07 21:05:46 -0500

    test: add route success test

--- server_test.go
+++ server_test.go
@@ -13,7 +13,8 @@ const metricsRoute string = "/metrics"
 func TestMetricsRouteFailure(t *testing.T) {
 	req := httptest.NewRequest(http.MethodPost, metricsRoute, nil)
 	w := httptest.NewRecorder()
-	MetricsRoute(w, req)
+	m := &MetricsCache{""}
+	m.MetricsRoute(w, req)
 	res := w.Result()
 	defer res.Body.Close()
 	data, err := ioutil.ReadAll(res.Body)
@@ -22,5 +23,21 @@ func TestMetricsRouteFailure(t *testing.T) {
 	}
 	if d := strings.TrimSuffix(string(data), "\n"); d != "Method Not Allowed" {
 		t.Errorf("expected 405 got '%s'", d)
+	}
+}
+
+func TestMetricsRouteSuccess(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, metricsRoute, nil)
+	w := httptest.NewRecorder()
+	m := &MetricsCache{"testMetric"}
+	m.MetricsRoute(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %s", err)
+	}
+	if d := strings.TrimSuffix(string(data), "\n"); d != m.Metrics {
+		t.Errorf("expected 200 got '%s'", d)
 	}
 }
